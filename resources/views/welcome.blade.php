cdc6cc66d2d582271ebc22021a993a54980a7933
AlbertWebs <albertmuhatia@gmail.com>
2024-10-24 12:00:35 +0300

    Update

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -658,7 +658,7 @@ You can find the code of your language here - https://www.w3schools.com/tags/ref
 
                             <div class="col-md-10 offset-md-1 col-lg-8 offset-lg-2">
 
-                                <h3 class="section-title text-center mb-80 mb-sm-60">
+                                <h3 class="section-title text-center mb-80 mb-sm-60" style="color: #c26940;">
                                     Frequently Asked Questions
                                 </h3>
 
@@ -666,28 +666,28 @@ You can find the code of your language here - https://www.w3schools.com/tags/ref
                                 <dl class="toggle">
 
                                     <dt>
-                                        <a href="#">What is BFL, and what services do you provide?</a>
+                                        <a href="#" style="color: #c26940;">What is BFL, and what services do you provide?</a>
                                     </dt>
                                     <dd class="black">
                                         FL is a [description of BFL's focus or industry, e.g., a financial services company, logistics firm, etc.]. We specialize in [list key services/products], including [examples of specific offerings that BFL provides].
                                     </dd>
 
                                     <dt>
-                                        <a href="#">How can i support BFL?</a>
+                                        <a style="color: #c26940;" href="#">How can i support BFL?</a>
                                     </dt>
                                     <dd class="black">
                                         To apply for [product/service], you can visit our website at businessforlife.com and fill out an online application form. Alternatively, you can contact us directly at [contact info] or visit our offices located at [physical address].
                                     </dd>
 
                                     <dt>
-                                        <a href="#">What is BFL's success rate?</a>
+                                        <a style="color: #c26940;" href="#">What is BFL's success rate?</a>
                                     </dt>
                                     <dd class="black">
                                         To apply for [product/service], you can visit our website at businessforlife.com and fill out an online application form. Alternatively, you can contact us directly at [contact info] or visit our offices located at [physical address].
                                     </dd>
 
                                     <dt>
-                                        <a href="#">How can BFL serve my organization?</a>
+                                        <a style="color: #c26940;" href="#">How can BFL serve my organization?</a>
                                     </dt>
                                     <dd class="black">
                                         The eligibility criteria depend on the specific product or service you're interested in. Generally, you need to provide [requirements, such as documentation, qualifications, etc.]. For detailed requirements, please visit [service/product page URL] or contact our customer service team.
